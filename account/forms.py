3f816d03b18876ec981d1dafaf9f0c8946400535
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-08-27 22:46:43 +0330

    not done

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
@@ -8,10 +8,14 @@ class UserRegistrationForm(forms.Form):
     password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
-
     def clean(self):
         cd = super().clean()
-        p1 = cd.get('password')
-        p2 = cd.get('confirm password')
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
         if p1 and p2 and p1 != p2:
-            raise
+            raise ValidationError('password must match')
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 class UserRegisterView(View):
@@ -17,7 +18,28 @@ class UserRegisterView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password1'])
             messages.success(request, 'you register successfully', 'success')
             return redirect('home:home')
         return render(request, self.template_name, {'form': form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name =
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'account/login.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you login successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'your username or password is wrong', 'danger')
+            return render(request, 'account/login.html', {'form': form})
+        return render(request, 'account/login.html', {'form': form})
