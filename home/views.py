797e3c54b62d931922f30215793ea0a848c5c36b
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-08-27 11:11:42 +0330

    create some html page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('hello user !')
+    return render(request, 'home/index.html')
