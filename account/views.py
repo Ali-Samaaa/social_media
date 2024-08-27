7b2fcac4165090820f525e65faee9f2d393a927f
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-08-27 22:05:04 +0330

    UserRegisterationView is not done1

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
-class RegisterView(View):
-    form_class = UserRegistrationForm()
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
     template_name = 'account/register.html'
 
     def get(self, request):
@@ -13,5 +15,9 @@ class RegisterView(View):
 
     def post(self, request):
         form = self.form_class(request.POST)
-
+        if form.is_valid():
+            cd = form.cleaned_data()
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            messages.success(request, 'you register successfully', 'success')
+            return redirect('home:home')
         return render(request, self.template_name, {'form': form})
