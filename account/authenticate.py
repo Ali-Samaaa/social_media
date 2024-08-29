76cbd2f64c152da6a2bcc86098bfda8f006d955e
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-08-29 11:22:44 +0330

    set a second authenticate backend

--- account/authenticate.py
+++ account/authenticate.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import login
-from django.contrib import messages
-from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
-            if user:
-                login(request, user)
-                messages.success(request, 'you login successfully', 'success')
-                return redirect('home:home')
-            else:
-                return None
+            if user.check_password(password):
+                return user
+            return None
+        except ObjectDoesNotExist:
+            return None
 
+    def get_user(self, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+            return user
+        except ObjectDoesNotExist:
+            return None
